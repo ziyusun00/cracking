1d3293738ac8355c4db5aca03678869833b69a13
ziyusunmao <78039863+ziyusunmao@users.noreply.github.com>
2022-02-22 13:36:04 -0600

    update

--- README.md
+++ README.md
@@ -1,17 +1,42 @@
 # Cracking Password
 ## Introduction:
 This project uses python to create a program that decrypt encrypted hexadecimal characters in order to determine the corresponding plain text password.
-Given a list of password digests (hashes.txt), this program should be able to cracking passwords one by one, and generate a output file (cracked.txt) with format < hash >:< password >. 
+Given a list of password digests (hashes.txt), this program should be able to cracking passwords one by one, 
+and generate a output file (cracked.txt) with format < hash >:< password >. 
 All of the hashes in input file were hashed using SHA256. All passwords are salted with the randomly generated salt (salt.txt). 
 
 ## Decryption Method:
+The first step is open the salt.txt and read the salt which is used to salt the possible password. 
+Opening the document hashes.txt to read decrypted characters in each line and save as all_password.
+
 ### 1. Dictionary attack
-Using rockyou.txt which is a common passwords file pull from internet.
-Using 20k.txt which is a common english words (no capitalization) file pull from internet.
-Using 11to26.txt which is a long english words (11-26 characters) file pull from internet.
+The core method of dictionary attack is to open txt files that pull from internet, use line.strip() to clear spaces at the end of each line of data, 
+and read characters in each line in turn, store them into variable test_password.
+These characters are possible password combinations. Salting them to get salted_test_password, 
+and then encoding them to UTF-8 using sha256 function in hashlib which returns hexadecimal hashed_test_password. 
+Compare the elements in hashed_test_password with the elements in all_password one by one. 
+If they are the same, print them in format < hashed_test_password >:< test_password >,
+and append the current element of hashed_test_password into pass_password.
+Finally, screening rest_password by selecting elements in all_password that are not 
+in pass_password to facilitate the continuation of other kinds of attack attempts.
+
+First, reading rockyou.txt file which is a common passwords file pull from internet.
+Secondly, reading 20k.txt file which is a common english words file pull from internet for first letter capitalization situation, 
+all uppercase letters situation, and all lowercase letters situation. Only first letter capitalization situation can decrypt passwords, so only keep it. 
+Finally, reading 11to26.txt file which is a long english words (11-26 characters) file pull from internet.
 
 ### 2. Brute force attack
-Try all random strings (up to 4 characters in lowercase letters, numbers) using python's itertools module which provides various functions that work on iterators to produce complex iterators.
+Try all random strings (up to 4 characters in lowercase letters, numbers) using python's itertools module, 
+which provides various functions that work on iterators to produce complex iterators, set the repeat time to 1 to 4 respectively.
+Storing these random strings into char_password, salting them to get salted_char_brute_password,
+and encoding them to UTF-8 using sha256 function in hashlib which returns hexadecimal hashed_char_brute_password.
+Compare the elements in hashed_char_brute_password with the elements in char_password one by one.
+If they are the same, print them in format < hashed_char_brute_password >:< char_password >, 
+and append the current element of hashed_char_brute_password into a new list named pass_password_2.
+Creating list named rest_password_2 to store passwords in rest_password but not in pass_password_2.
 
 ## Conclusion：
-A total of 46 of the 55 codes were successfully cracked.
+A total of 49 of the 55 codes were successfully cracked.
+All decryption results are printed on the console as < hashed_test_password >:< test_password > during the program run. 
+The unsuccessfully decrypted passwords are also printed on the console as < rest_password >:< space >.
+Copy and paste all the console run results into an output file named cracked.txt that clearly shows the decrypted results
--- cracking.py
+++ cracking.py
@@ -1,11 +1,17 @@
-import itertools, hashlib, math
+import itertools, hashlib, math, re
 
 # read salt.txt
 with open("/Users/sunziyu/Desktop/salt.txt", "r") as f:
   salt = f.read().strip()
 
+# store all the passwords into list all_password
+all_password = []
+with open("/Users/sunziyu/Desktop/hashes.txt", "r") as k:
+  for line in k:
+    real_password = line.strip()
+    all_password.append(real_password)
+
 # dictionary attack
-# store the cracked passwords into list all_password
 pass_password = []
 with open("/Users/sunziyu/Desktop/rockyou.txt", "r", errors='ignore') as f:
   for line in f:
@@ -15,43 +21,96 @@ with open("/Users/sunziyu/Desktop/rockyou.txt", "r", errors='ignore') as f:
     salted_test_password = (salt + test_password).encode('utf-8')
     # hash salted password
     hashed_test_password = hashlib.sha256(salted_test_password).hexdigest()
-    with open("/Users/sunziyu/Desktop/hashes.txt", "r") as k:
-      for line in k:
-        real_password = line.strip()
-        if real_password == hashed_test_password:
-          pass_password.append(real_password)
-          print(real_password + ":" + test_password)
-
-# store all the passwords into list all_password
-all_password = []
-with open("/Users/sunziyu/Desktop/hashes.txt", "r") as k:
-  for line in k:
-    real_password = line.strip()
-    all_password.append(real_password)
-
-# two lists both have exist_password
-exist_password = [x for x in all_password if x in pass_password]
+    for i in all_password:
+      if i == hashed_test_password:
+        if i not in pass_password:
+          pass_password.append(i)
+          print(i + ":" + test_password)
 # rest_password list is the rest of passwords that weren't cracked
-rest_password = [y for y in (all_password + pass_password) if y not in exist_password]
+rest_password = [y for y in all_password if y not in pass_password]
 
 # brute-force attack
 chars = '01234567890abcdefghijklmnopqrstuvwxyz'
-pass_password = []
+char_password = []
 for i in itertools.product(chars, repeat=4):
-    pass_password.append(''.join(i))
+    char_password.append(''.join(i))
 for i in itertools.product(chars, repeat=3):
-    pass_password.append(''.join(i))
+    char_password.append(''.join(i))
 for i in itertools.product(chars, repeat=2):
-    pass_password.append(''.join(i))
+    char_password.append(''.join(i))
 for i in itertools.product(chars, repeat=1):
-    pass_password.append(''.join(i))
+    char_password.append(''.join(i))
 
-#check if brute froce cracked any password
-for x in pass_password:
-    salted_test_brute_password = (salt + x).encode('utf-8')
-    hashed_test_brute_password = hashlib.sha256(salted_test_brute_password).hexdigest()
+pass_password_2 = []
+for x in char_password:
+    salted_char_brute_password = (salt + x).encode('utf-8')
+    hashed_char_brute_password = hashlib.sha256(salted_char_brute_password).hexdigest()
     for y in rest_password:
-        if hashed_test_brute_password == y:
-            print(y + ":" + x)
+        if hashed_char_brute_password == y:
+          print(y + ":" + x)
+          pass_password_2.append(y)
+# rest_password_2 list is the rest of passwords that weren't cracked
+rest_password_2 = [y for y in rest_password if y not in pass_password_2]
 
+# dictionary attack
+pass_password_3 = []
+with open("/Users/sunziyu/Desktop/20k.txt", "r", errors='ignore') as f:
+  for line in f:
+    # strip new line character
+    file_password = line.strip()
+    file_password = file_password.capitalize()
+    # concat salt and password, encode to utf-8
+    salted_file_password = (salt + file_password).encode('utf-8')
+    # hash salted password
+    hashed_file_password = hashlib.sha256(salted_file_password).hexdigest()
+    for y in rest_password_2:
+        if y == hashed_file_password:
+          pass_password_3.append(hashed_file_password)
+          print(y + ":" + file_password)
+# rest_password_3 list is the rest of passwords that weren't cracked
+rest_password_3 = [y for y in rest_password_2 if y not in pass_password_3]
 
+# dictionary attack
+pass_password_4 = []
+with open("/Users/sunziyu/Desktop/11to26.txt", "r", errors='ignore') as s:
+  for line in s:
+    # strip new line character
+    long_password = line.strip()
+    salted_long_password = (salt + long_password).encode('utf-8')
+    hashed_long_password = hashlib.sha256(salted_long_password).hexdigest()
+    for y in rest_password_3:
+        if y == hashed_long_password:
+          pass_password_4.append(hashed_long_password)
+          print(y + ":" + long_password)
+# rest_password_4 list is the rest of passwords that weren't cracked
+rest_password_4 = [y for y in rest_password_3 if y not in pass_password_4]
+
+for i in rest_password_4:
+  print(i + ":")
+
+'''''
+pass_password_5 = []
+symbols = '!@#$%^&*-+=<>?'
+with open("/Users/sunziyu/Desktop/20k.txt", "r", errors='ignore') as f:
+  for line in f:
+    # strip new line character
+    name_password = line.strip()
+    for i in itertools.product(symbols, repeat=1):
+      name_password+''.join(i)
+      salted_name_password = (salt + name_password).encode('utf-8')
+      hashed_name_password = hashlib.sha256(salted_name_password).hexdigest()
+      for y in rest_password_4:
+          if y == hashed_name_password:
+            pass_password_5.append(hashed_name_password)
+            print(y + ":" + name_password)
+
+two_words_password = []
+for i in itertools.product(common_words, repeat=2):
+    two_words_password.append(''.join(i))
+for x in two_words_password:
+    salted_two_words_password = (salt + x).encode('utf-8')
+    hashed_two_words_password = hashlib.sha256(salted_two_words_password).hexdigest()
+    for y in rest_password_4:
+        if hashed_two_words_password == y:
+          print(y + ":" + x)
+'''''
